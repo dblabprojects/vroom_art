e68148aa6536ca41020d1a026e65f26a0c1ccb30
Leonardo Azzi Martins <martinsl@dbserver.com.br>
2021-11-16 14:49:49 -0300

    a

--- cheatsheet.js
+++ cheatsheet.js
@@ -1,2 +1,3 @@
-let sceneEl = document.querySelector('a-scene')
+const sceneEl = document.querySelector('a-scene')
 const iot = sceneEl.querySelector('#iot_lab');
+iot.setAttribute('scale', '0.5 0.5 0.5')
